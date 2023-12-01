33655dc2fe2448d4ac7df1f0d7accc23f59f1f45
Pirantel <max420652@gmail.com>
2023-12-02 03:31:20 +0500

    Refactor URL configuration and add test view

--- HWTracker/main/urls.py
+++ HWTracker/main/urls.py
@@ -1,7 +1,8 @@
+# main/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('social-auth/complete/google-oauth2/', views.test, name='google-auth-complete'),
     path('', views.index, name='index'),
-    
 ]
