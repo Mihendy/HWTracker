6f78cff54b2a4b39cb8bebb0f5e9e2053f7a1590
Pirantel <max420652@gmail.com>
2023-12-03 01:26:36 +0500

    Add session handling for username

--- HWTracker/main/views.py
+++ HWTracker/main/views.py
@@ -4,21 +4,27 @@ from urllib.parse import urlparse, parse_qs
 import jwt
 
 def index(request):
+    username = request.session.get('username')
+    if username is not None:
+        return redirect('student')
     return render(request, 'main/index.html')
 
 def student(request):
-    username = 'your_username'
+    username = request.session.get('username')
+    if username is None:
+        return redirect('/')
     return render(request, 'main/student.html', {'username': username})
 
 def handle_auth(request):
     code = parse_qs(urlparse(request.get_full_path()).query)['code'][0]
     data = get_user_information(code)
     decoded = jwt.decode(data['id_token'], '', algorithms='none', options={'verify_signature': False})
-    print(decoded)
+    request.session['username'] = decoded['name']
     return redirect('student')
 
 def get_user_information(code):
-    request = requests.post('https://oauth2.googleapis.com/token', data={
+    request = requests.post('https://oauth2.googleapis.com/token', 
+    data={
         'code': code,
         'client_id': '437781818230-4tdb2qsrg7qhlmu5ud8dbge55mf8e79k.apps.googleusercontent.com',
         'client_secret': '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',
