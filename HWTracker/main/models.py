ceaa33044d98b065a1fb886337afb35ae3b319e6
Pirantel <max420652@gmail.com>
2023-12-03 05:14:56 +0500

    Updated grid

--- HWTracker/main/models.py
+++ HWTracker/main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Task(models.Model):
     subject = models.CharField(max_length=50)
     topic = models.CharField(max_length=50)
-    description = models.TextField()
+    description = models.TextField(max_length=150)
     due_date = models.DateTimeField()
     completed = models.BooleanField(default=False)
     def __str__(self):
