846d38606f4f5d91e30bd5107eab5d7a6ba442d3
mihendy <kryukovm1khael@yandex.ru>
2023-12-09 02:02:00 +0500

    user update

--- HWTracker/main/models.py
+++ HWTracker/main/models.py
@@ -16,3 +16,16 @@ class Task(models.Model):
 
     class Meta:
         ordering = ['due_date']
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    # def get_user_count(self):
+    #     return self.users.count()
+
+    class Meta:
+        pass
---
+++ HWTracker/users/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    name = 'users'
+    verbose_name = 'Пользователь'
+    verbose_name_plural = 'Пользователи'
