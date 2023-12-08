bbcf3d0afa69208ff63b4cf9652b4a62912292e6
IlyaTucha <atuchin04@yandex.ru>
2023-12-08 23:40:06 +0500

    changed taskform

--- HWTracker/main/forms.py
+++ HWTracker/main/forms.py
@@ -4,14 +4,13 @@ from django import forms
 
 from .models import Task
 
-
 class TaskForm(forms.Form):
-    subject = forms.CharField(label='Предмет')
-    topic = forms.CharField(max_length=32, label='Тип')
-    description = forms.CharField(widget=forms.Textarea, label='Описание')
+    subject = forms.CharField(label='Предмет', widget=forms.TextInput(attrs={'class': 'field-of-subject'}))
+    topic = forms.CharField(label='Тип', widget=forms.TextInput(attrs={'class': 'field-of-topic'}))
+    description = forms.CharField(label='Описание', widget=forms.Textarea(attrs={'class': 'field-of-description'}))
     due_date = forms.CharField(
-        widget=forms.TextInput(attrs={'type': 'datetime-local'}),
-        label='Сдать до')
+        widget=forms.TextInput(attrs={'type': 'datetime-local', 'class': 'field-of-due_date'}),
+        label='Сдать до',)
 
     class Meta:
         model = Task
